f7116dd1d15a16c092b3756424faea80dc448de6
Luis Morales <luismorales@MacBook-Pro-de-Luis-Morales.local>
2014-04-27 15:05:15 +0200

    First Release

---
+++ Readme.md
@@ -0,0 +1,6 @@
+## Readme.md
+One of the most exciting areas in all of data science right now is wearable computing. 
+
+Companies like Fitbit, Nike, and Jawbone Up are racing to develop the most advanced algorithms to attract new users. The data used in this project represent data collected from the accelerometers from the Samsung Galaxy S smartphone.
+
+The objective of this project is to obtain a tidy data set with the average of all the features for each of the subjects in the experiment. 
---
+++ run_analysis.R
@@ -0,0 +1,49 @@
+# Peer Assingment. Getting and Cleaning Data
+# FIRST. TIDY SUBSET of DATA (MEAN and STD)
+# Load train set
+trainData <- read.table("./dataset/train/X_train.txt")
+
+# Load test set
+testData <- read.table("./dataset/test/X_test.txt")
+
+# Merge the training and the test sets to create one data set
+allData <- rbind(trainData,testData)
+
+# Load features.txt to obtain all the columns that are mean or
+# standard deviation from the different variables
+featuresTable <- read.table("./dataset/features.txt")
+features <- featuresTable$V2
+
+# Extract only the measurements on the mean and standard deviation for each measurement
+tempFeatures <- lapply(c("mean()","std()"),grep,features,fixed=TRUE)
+meanstdCols <- sort(c(tempFeatures[[1]],tempFeatures[[2]]))
+meanstdData <- allData[,meanstdCols]
+
+# Label the data set with descriptive activity names
+names(meanstdData) <- features[meanstdCols]
+
+#SECOND. TIDY SET, AVERAGE FOR EACH ACTIVITY AND SUBJECT
+# Name allData variables properly
+names(allData) <- features
+# Load the subject for each row
+subjectsTrain <- readLines("./dataset/train/subject_train.txt")
+subjectsTest <- readLines("./dataset/test/subject_test.txt")
+
+subjects <- as.numeric(c(subjectsTrain,subjectsTest))
+
+# Build the mean table
+meanData <- data.frame(matrix(nrow=length(unique(subjects)),ncol=length(features)+1))
+
+for (i in 1:length(features)) {
+  meanData[,i+1] <- tapply(allData[,i],as.numeric(subjects),mean)
+}
+
+# Set subjects
+names(meanData) <- c("subject",as.vector(features))
+
+# Write a file with the avg data
+write.table(meanData,file="./avg-data.txt")
+
+
+
+
